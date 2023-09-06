93c863a01dc714e4f975eca9955182536b71c3fe
Jordan Harband <ljharb@gmail.com>
2023-09-06 11:42:49 -0700

    [Deps] update `define-properties`, `es-abstract`

--- implementation.js
+++ implementation.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var ArrayCreate = require('es-abstract/2022/ArrayCreate');
-var CreateDataPropertyOrThrow = require('es-abstract/2022/CreateDataPropertyOrThrow');
-var Get = require('es-abstract/2022/Get');
-var LengthOfArrayLike = require('es-abstract/2022/LengthOfArrayLike');
-var ToObject = require('es-abstract/2022/ToObject');
-var ToString = require('es-abstract/2022/ToString');
+var ArrayCreate = require('es-abstract/2023/ArrayCreate');
+var CreateDataPropertyOrThrow = require('es-abstract/2023/CreateDataPropertyOrThrow');
+var Get = require('es-abstract/2023/Get');
+var LengthOfArrayLike = require('es-abstract/2023/LengthOfArrayLike');
+var ToObject = require('es-abstract/2023/ToObject');
+var ToString = require('es-abstract/2023/ToString');
 
 module.exports = function toReversed() {
 	var O = ToObject(this); // step 1
